4e83e207c49a7021c8da3e7f63e81652e73043af
Phil Barresi <me@philbarresi.com>
2016-07-28 22:35:09 -0400

    Added lecture 11 code

---
+++ Lecture Code/lecture_11/routes/index.js
@@ -0,0 +1,16 @@
+const apiRoutes = require("./api");
+const todoData = require('../data');
+
+const constructorMethod = (app) => {
+    app.use("/api", apiRoutes);
+
+    app.get("/", function (request, response) {
+        response.render("home", { pageTitle: "So Much ToDo!", todoItems: todoData.getAll() });
+    });
+
+    app.use("*", (req, res) => {
+        res.sendStatus(404);
+    })
+};
+
+module.exports = constructorMethod;
