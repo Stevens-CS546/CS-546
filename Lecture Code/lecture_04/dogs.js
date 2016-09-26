67142fef429ef68de2c2da97e181ed5d4d36e36c
Phil Barresi <me@philbarresi.com>
2016-09-26 16:11:06 -0400

    Updated error catching in dogs file

--- Lecture Code/lecture_04/dogs.js
+++ Lecture Code/lecture_04/dogs.js
@@ -5,41 +5,72 @@ let exportedMethods = {
     // This is a fun new syntax that was brought forth in ES6, where we can define
     // methods on an object with this shorthand!
     getDogById(id) {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
+        
         return dogs().then((dogCollection) => {
-            return dogCollection.findOne({ _id: id });
+            return dogCollection.findOne({_id: id});
         });
     },
     addDog(name, breeds) {
+        if (!name) 
+            return Promise.reject("You must provide a name for your dog");
+        
+        if (!breeds || !Array.isArray(breeds)) 
+            return Promise.reject("You must provide an array of breeds");
+        
+        if (breeds.length === 0) 
+            return Promise.reject("You must provide at least one breed.");
+        
         return dogs().then((dogCollection) => {
             let newDog = {
                 name: name,
                 breeds: breeds
             };
 
-            return dogCollection.insertOne(newDog).then((newInsertInformation) => {
-                return newInsertInformation.insertedId;
-            }).then((newId) => {
-                return this.getDogById(newId);
-            });
+            return dogCollection
+                .insertOne(newDog)
+                .then((newInsertInformation) => {
+                    return newInsertInformation.insertedId;
+                })
+                .then((newId) => {
+                    return this.getDogById(newId);
+                });
         });
     },
     removeDog(id) {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
+        
         return dogs().then((dogCollection) => {
-            return dogCollection.removeOne({ _id: id }).then((deletionInfo) => {
-                if (deletionInfo.deletedCount === 0) {
-                    throw (`Could not delete dog with id of ${id}`)
-                }
-            });
+            return dogCollection
+                .removeOne({_id: id})
+                .then((deletionInfo) => {
+                    if (deletionInfo.deletedCount === 0) {
+                        throw(`Could not delete dog with id of ${id}`)
+                    }
+                });
         });
     },
     updateDog(id, name, breeds) {
+        if (!id) 
+            return Promise.reject("You must provide an id to search for");
+        
+        if (!breeds || !Array.isArray(breeds)) 
+            return Promise.reject("You must provide an array of breeds");
+        
+        if (breeds.length === 0) 
+            return Promise.reject("You must provide at least one breed.");
+        
         return dogs().then((dogCollection) => {
             let updatedDog = {
                 name: name,
                 breeds: breeds
             };
 
-            return dogCollection.updateOne({ _id: id }, updatedDog).then(() => {
+            return dogCollection.updateOne({
+                _id: id
+            }, updatedDog).then(() => {
                 return this.getDogById(id);
             });
         });
