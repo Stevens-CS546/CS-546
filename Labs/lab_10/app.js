821ce2ee428b27044180e569157573df8c63e6e0
Phil Barresi <me@philbarresi.com>
2017-04-21 11:31:15 -0400

    Added lab 10 code

---
+++ Labs/lab_10/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  const email = req.body.email;
+  const message = req.body.text;
+
+  const dataResponse = {
+    email: email,
+    message: message,
+    reply: `[${email}]: ${message}`
+  };
+
+  res.json(JSON.stringify(dataResponse));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
