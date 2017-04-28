49fe513f25c2db302589da4e7eeec7cd73599e06
Phil Barresi <me@philbarresi.com>
2017-04-28 07:41:30 -0400

    Updated lecture 13 a11y css3 repo

---
+++ Lecture Code/lecture_13/accessibility_and_css3/app.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it");
+});
