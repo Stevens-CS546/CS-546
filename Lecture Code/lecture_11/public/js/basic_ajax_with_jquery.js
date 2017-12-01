ed62e0d8bc46bc90095114040ed56c47d9ea759f
Phil Barresi <me@philbarresi.com>
2017-12-01 11:38:31 -0500

    Updated AJAX examples

--- Lecture Code/lecture_11/public/js/basic_ajax_with_jquery.js
+++ Lecture Code/lecture_11/public/js/basic_ajax_with_jquery.js
@@ -1,35 +1,78 @@
-(function ($) {
-    // Let's start writing AJAX calls!
+(function($) {
+  // Let's start writing AJAX calls!
 
-    var myNewTaskForm = $("#new-item-form"),
-        newNameInput = $("#new-task-name"),
-        newDecriptionArea = $("#new-task-description");
+  var myNewTaskForm = $("#new-item-form"),
+    newNameInput = $("#new-task-name"),
+    newDecriptionArea = $("#new-task-description"),
+    todoArea = $("#todo-area");
 
-    myNewTaskForm.submit(function (event) {
-        event.preventDefault();
+  function bindEventsToTodoItem(todoItem) {
+    todoItem.find(".finishItem").on("click", function(event) {
+      event.preventDefault();
+      var currentLink = $(this);
+      var currentId = currentLink.data("id");
 
-        var newName = newNameInput.val();
-        var newDescription = newDecriptionArea.val();
-        var newContent = $("#new-content");
+      var requestConfig = {
+        method: "POST",
+        url: "/api/todo/complete/" + currentId
+      };
 
-        if (newName && newDescription) {
-            var requestConfig = {
-                method: "POST",
-                url: "/api/todo",
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    name: newName,
-                    description: newDescription,
-                    testField: 12,
-                    testBool: true
-                })
-            };
-
-            $.ajax(requestConfig).then(function (responseMessage) {
-                console.log(responseMessage);
-                newContent.html(responseMessage.message);
-                //                alert("Data Saved: " + msg);
-            });
-        }
+      $.ajax(requestConfig).then(function(responseMessage) {
+        var newElement = $(responseMessage);
+        bindEventsToTodoItem(newElement);
+        todoItem.replaceWith(newElement);
+      });
     });
+  }
+
+  todoArea.children().each(function(index, element) {
+    bindEventsToTodoItem($(element));
+  });
+
+  myNewTaskForm.submit(function(event) {
+    event.preventDefault();
+
+    var newName = newNameInput.val();
+    var newDescription = newDecriptionArea.val();
+    var newContent = $("#new-content");
+
+    if (newName && newDescription) {
+      var useJson = false;
+      if (useJson) {
+        var requestConfig = {
+          method: "POST",
+          url: "/api/todo",
+          contentType: "application/json",
+          data: JSON.stringify({
+            name: newName,
+            description: newDescription
+          })
+        };
+
+        $.ajax(requestConfig).then(function(responseMessage) {
+          console.log(responseMessage);
+          newContent.html(responseMessage.message);
+          //                alert("Data Saved: " + msg);
+        });
+      } else {
+        var requestConfig = {
+          method: "POST",
+          url: "/api/todo.html",
+          contentType: "application/json",
+          data: JSON.stringify({
+            name: newName,
+            description: newDescription
+          })
+        };
+
+        $.ajax(requestConfig).then(function(responseMessage) {
+          console.log(responseMessage);
+          var newElement = $(responseMessage);
+          bindEventsToTodoItem(newElement);
+
+          todoArea.append(newElement);
+        });
+      }
+    }
+  });
 })(window.jQuery);
--- Lecture Code/lecture_11/routes/api.js
+++ Lecture Code/lecture_11/routes/api.js
@@ -1,15 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const xss = require('xss');
+const todoData = require("../data");
+const xss = require("xss");
 
-router.post("/todo", function (request, response) {
-    console.log(request.body);
-    response.json({ success: true, message: xss(request.body.description) });
+router.post("/todo", function(request, response) {
+  todoData.makeToDo(request.body.name, request.body.description);
+
+  //response.json({ success: true, message: xss(request.body.description) });
+  response.json({ success: true, message: xss(request.body.description) });
 });
 
-router.post("/todo.html", function (request, response) {
-    console.log(request.body);
-    response.send("<div>" + xss(request.body.description) + "</div>");
+router.post("/todo/complete/:id", function(request, response) {
+  const updatedData = todoData.finishToDo(parseInt(request.params.id));
+  response.render("partials/todo_item", { layout: null, ...updatedData });
+});
+
+/*
+router.post("/todo.html", function(request, response) {
+  console.log(request.body);
+
+  todoData.makeToDo(request.body.name, request.body.description);
+
+  response.send("<div>" + xss(request.body.description) + "</div>");
+});*/
+
+router.post("/todo.html", function(request, response) {
+  const newTodo = todoData.makeToDo(
+    request.body.name,
+    request.body.description
+  );
+
+  response.render("partials/todo_item", { layout: null, ...newTodo });
 });
 
 module.exports = router;
