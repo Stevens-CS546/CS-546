ffbdecf7e8994cecf7064c5bbe30a2995107c539
Phil Barresi <me@philbarresi.com>
2018-02-23 12:13:24 -0500

    Some basic syntax formatting

--- Lecture Code/lecture_06/routes/index.js
+++ Lecture Code/lecture_06/routes/index.js
@@ -1,13 +1,13 @@
 const postRoutes = require("./posts");
 const userRoutes = require("./users");
 
-const constructorMethod = (app) => {
-    app.use("/posts", postRoutes);
-    app.use("/users", userRoutes);
+const constructorMethod = app => {
+  app.use("/posts", postRoutes);
+  app.use("/users", userRoutes);
 
-    app.use("*", (req, res) => {
-        res.status(404).json({error: "Not found"});
-    });
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
 };
 
 module.exports = constructorMethod;
