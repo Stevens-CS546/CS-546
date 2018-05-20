38f1dbb2683344b4ba4f0c5b4e71320ef04383f5
Phil Barresi <me@philbarresi.com>
2018-05-20 10:11:58 -0400

    Added newlines to template string

--- Lecture Code/lecture_01/strings.js
+++ Lecture Code/lecture_01/strings.js
@@ -2,7 +2,12 @@ let string1 = "This is the first string; it's block scoped.";
 const string2 = "This is the second string; it is a constant.";
 // NOTE: SOME AUTO FORMATTERS WILL CONVERT THESE TO DOUBLE QUOTES:
 var string3 = 'This is the "third string"; it\'s functionally scoped';
-let string4 = `string1 is: ${string1}`;
+let string4 = `string1 is: 
+
+
+
+
+${string1} after some newlines`;
 
 console.log(string1);
 console.log(string2);
